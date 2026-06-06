9bec2af93d208e95ed00e899701b3ad37c995b8e
TEC Capital web <teccapitalweb@gmail.com>
2026-06-06 10:11:23 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* IMDAC · Club — Service Worker v8 */
-const CACHE='imdac-club-v8';
+/* IMDAC · Club — Service Worker v9 */
+const CACHE='imdac-club-v9';
 const ASSETS=['./','./index.html','./app.js','./cert-font.js','./manifest.json','./assets/logo-imdac.png'];
 
 self.addEventListener('install',e=>{
